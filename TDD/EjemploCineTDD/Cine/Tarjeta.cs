8ca418582b69d4337d9f729cb950b9e56550c71d
Mario Souto <msoutoolazabal@gmail.com>
2020-09-07 22:46:15 -0300

    Clase 07/09

---
+++ TDD/EjemploCineTDD/Cine/Tarjeta.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cine
+{
+    public class Tarjeta
+    {
+        public string Marca { get; set; }
+        public double Descuento { get; set; }
+    }
+}
---
+++ TDD/EjemploCineTDD/CineTest/CarritoTest.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Cine;
+
+namespace CineTest
+{
+    [TestClass]
+    public class CarritoTest
+    {
+
+        [TestMethod]
+        public void CompraRegularDeDosEntradasTest()
+        {
+            Carrito carrito = new Carrito();
+            carrito.Agregar(new Entrada() { Monto = 150, Pelicula = "Star Wars" });
+            carrito.Agregar(new Entrada() { Monto = 150, Pelicula = "Star Wars" });
+
+            int montoTotal = carrito.Checkout();
+
+            Assert.AreEqual(300, montoTotal);
+        }
+
+        [TestMethod]
+        public void CompraDeDosEntradasConTarjeta()
+        {
+            Carrito carrito = new Carrito();
+            carrito.Agregar(new Entrada() { Monto = 150, Pelicula = "Star Wars" });
+            carrito.Agregar(new Entrada() { Monto = 150, Pelicula = "Star Wars" });
+
+            int montoTotal = carrito.Checkout(new Tarjeta { Marca = "ITAU", Descuento = 0.5 });
+
+            Assert.AreEqual(150, montoTotal);
+        }
+    }
+}
